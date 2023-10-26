a048ccf8f097f2aff292d72709f16f7734c8a9e3
HoppXR <jaden.chen1@ontariotechu.net>
2023-10-26 19:20:32 -0400

    Added Assets

--- Assets/Scripts/GameSpace.cs
+++ Assets/Scripts/GameSpace.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameSpace : MonoBehaviour
 {
-    public GameObject DiseaseCube;
+    [SerializeField] private int diseaseCubes;
+
+    [SerializeField] private Sprite dc1, dc2, dc3;
 
     void Start()
     {
 
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
